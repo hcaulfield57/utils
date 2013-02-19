263123747e8a9a60c891e6ada902018c18a1bac8
hcaulfield57 <hcaulfield57@gmail.com>
2013-02-19 10:52:52 -0700

    rewrote code

--- newmail/newmail.c
+++ newmail/newmail.c
@@ -16,61 +16,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <regex.h>
 
-int count_msg(char *spoolfile);
-char *userfmt(char *fmt, ...);
-
-int msg;
-extern char **environ;
+void usage(void);
+int count(char *spoolfile);
 
 int
 main(int argc, char *argv[])
 {
-	char user[25], spool[104];	
+	char *spool;
 
 	if(argc == 3 && !strcmp(argv[1], "-s"))
-		strncpy(spool, argv[2], 104);
+		spool = argv[2];
 	else if(argc == 1) {
-		if(getenv("MAIL") == NULL) {
-			fprintf(stderr, "Usage %s: [ -s spoolfile ]\n", argv[0]);
-			exit(1);
-		}
-		strncpy(spool, getenv("MAIL"), 104);
-	} else {
-		fprintf(stderr, "Usage %s: [ -s spoolfile ]\n", argv[0]);
-		exit(1);
-	}
+		spool = getenv("MAIL");
+		if(spool == NULL)
+			usage();
+	} else
+		usage();
 
-	printf("%d\n", count_msg(spool));
-
+	printf("%d\n", count(spool));
 	return 0;
 }
 
 int
-count_msg(char *spoolfile)
+count(char *spoolfile)
 {
-	FILE *fp;
-	int from;
-	regex_t preg;
+	int reg, msg;
 	char buf[1024];
+	regex_t preg;
+	FILE *fp;
 
-	msg = 0;
 	if((fp = fopen(spoolfile, "r")) == NULL) {
 		fprintf(stderr, "can't open spoolfile: %s\n", spoolfile);
 		exit(1);
 	}
+	msg = 0;
 	while(fgets(buf, 1024, fp) != NULL) {
-		if((from = regcomp(&preg, "^From:.*", 0))) {
-			fprintf(stderr, "could not compile regular expression\n");
+		if((reg = regcomp(&preg, "^From:.*", 0)) != 0) {
+			fprintf(stderr, "could not compile regex\n");
 			exit(1);
 		}
-		if(!(from = regexec(&preg, buf, 0, NULL, 0)))
+		if(!(reg = regexec(&preg, buf, 0, NULL, 0)))
 			msg++;
 	}
-
 	return msg;
+}
+
+void
+usage(void)
+{
+	fprintf(stderr, "Usage newmail: [ -s spoolfile ]\n");
+	exit(1);
 }
