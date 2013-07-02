5637def150de3401a33ab46cdfe2bc2c06bce817
Grant Mather <hcaulfield57@gmail.com>
2013-07-02 16:14:24 -0600

    modified newmail

--- newmail/newmail.c
+++ newmail/newmail.c
@@ -1,11 +1,11 @@
-/* Copyright (C) 2013, Grant Mather <hcaulfield57@gmail.com>
- * 
+/* Copyright (C) 2013 Grant Mather <hcaulfield57@gmail.com>
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies. 
+ * copyright notice and this permission notice appear in all copies.
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF 
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
@@ -13,61 +13,67 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <regex.h>
 
+int count(char *spool);
 void usage(void);
-int count(char *spoolfile);
 
 int
 main(int argc, char *argv[])
 {
-	char *spool;
+    char *spool;
+    char buf[512];
 
-	if(argc == 3 && !strcmp(argv[1], "-s"))
-		spool = argv[2];
-	else if(argc == 1) {
-		spool = getenv("MAIL");
-		if(spool == NULL)
-			usage();
-	} else
-		usage();
+    spool = NULL;
+    memset(buf, 0, 512);
+    if(argc == 3 && !strcmp(argv[1], "-s"))
+        spool = argv[2];
+    else if(argc == 1) {
+        spool = getenv("MAIL");
+        if(spool == NULL) {
+            strncat(buf, "/var/mail/", 512);
+            strncat(buf, getenv("USER"), 512); 
+            spool = buf;
+        }
+    } else
+        usage();
 
-	printf("%d\n", count(spool));
-	return 0;
+    printf("%d\n", count(spool));
+    return 0;
 }
 
 int
-count(char *spoolfile)
+count(char *spool)
 {
-	int reg, msg;
-	char buf[1024];
-	regex_t preg;
-	FILE *fp;
+    int msg;
+    FILE *fp;
+    char buf[1024];
 
-	if((fp = fopen(spoolfile, "r")) == NULL) {
-		fprintf(stderr, "can't open spoolfile: %s\n", spoolfile);
-		exit(1);
-	}
-	msg = 0;
-	while(fgets(buf, 1024, fp) != NULL) {
-		if((reg = regcomp(&preg, "^From:.*", 0)) != 0) {
-			fprintf(stderr, "could not compile regex\n");
-			exit(1);
-		}
-		if(!(reg = regexec(&preg, buf, 0, NULL, 0)))
-			msg++;
-	}
-	return msg;
+    msg = 0;
+    if((fp = fopen(spool, "r")) == NULL)
+        err(1, "cannot open %s", spool);
+    while(fgets(buf, 1024, fp) != NULL) {
+        if(buf[0] != 'F')
+            continue;
+        else if(buf[1] != 'r')
+            continue;
+        else if(buf[2] != 'o')
+            continue;
+        else if(buf[3] != 'm')
+            continue;
+        else if(buf[4] != ' ')
+            continue;
+        msg++;
+    }
+
+    return msg;
 }
 
 void
 usage(void)
 {
-	fprintf(stderr, "Usage newmail: [ -s spoolfile ]\n");
-	exit(1);
+    err(1, "usage: newmail [-s spool]\n");
 }
