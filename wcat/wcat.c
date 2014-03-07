2b545c613b57a58afaf2f4ba465b62b8074d6c12
Grant Mather <hcaulfield57@gmail.com>
2014-03-07 08:32:48 -0700

    fixed some stupid mistakes, still doesn't work

--- wcat/wcat.c
+++ wcat/wcat.c
@@ -7,8 +7,6 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
-char *request(const char *s);
-
 int
 main(int argc, char *argv[])
 {
@@ -25,9 +23,9 @@ main(int argc, char *argv[])
 
 	for(i = 1; i < argc; i++) {
 		memset(&hints, 0, sizeof hints);
-		hints.ai_flags = AF_UNSPEC;
-		hints.ai_family = SOCK_STREAM;
-		if(getaddrinfo(argv[i], "http", &hints, &res) == -1)
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+		if((error = getaddrinfo(argv[i], "http", &hints, &res)) != 0)
 			errx(1, "getaddrinfo error: %s", gai_strerror(error));
 		for(p = res; p; p = p->ai_next) {
 			if((sockfd = socket(p->ai_family, p->ai_socktype, 
@@ -38,23 +36,12 @@ main(int argc, char *argv[])
 				break;
 		}
 		freeaddrinfo(res);
-		strlcpy(buf, request(argv[i]), sizeof buf);
+                snprintf(buf, sizeof buf, "GET /index.html HTTP/1.1\r\n");
 		if(send(sockfd, buf, sizeof buf, 0) == -1)
 			err(1, "send error");
 
-		if(recv(sockfd, buf, sizeof buf, 0) == -1)
-			err(1, "recv error");
-
-		printf("%s", buf);
+		while(recv(sockfd, buf, sizeof buf, 0) > 0)
+                    printf("%s", buf);
 	}
 	return 0;
-}
-
-char
-*request(const char *s)
-{
-	static char buf[1024];
-
-	snprintf(buf, 1024, "GET %s HTTP/1.0", s);
-	return buf;
 }
