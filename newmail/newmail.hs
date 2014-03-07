b45dff5cc0b5751bea1d5d7a40a1bac147ede60e
Grant Mather <hcaulfield57@gmail.com>
2014-03-06 21:33:29 -0700

    added new version

---
+++ newmail/newmail.hs
@@ -0,0 +1,33 @@
+import Control.Monad.Writer
+import Data.List
+import System.Directory
+import System.Environment
+import System.Exit
+import System.IO
+
+count :: Handle -> WriterT (Sum Int) IO ()
+count h = do
+    line <- lift $ hGetLine h
+    eof <- lift $ hIsEOF h
+    if eof then return ()
+        else if isPrefixOf "From " line 
+            then tell (Sum 1) >> count h
+            else count h
+
+main :: IO ()
+main = do
+    argv <- getArgs
+    mail <- getEnv "MAIL"
+    let spool = case argv of
+            ("-s":sf:_) -> sf
+            _ -> mail
+    isFile <- doesFileExist spool
+    if isFile then do
+        handle <- openFile spool ReadMode
+        num <- execWriterT (count handle)
+        print $ getSum num
+        else usage
+
+usage :: IO ()
+usage = putStrLn "usage: newmail [-s spool]"
+    >> exitWith (ExitFailure 1)
